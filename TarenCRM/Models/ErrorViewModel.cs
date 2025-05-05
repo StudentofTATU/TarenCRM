1562a23b5a2389976ab121cf84467e9cdadc1ce1
Acer <Acer@DESKTOP-4RKCN28>
2025-05-05 09:21:08 +0500

    Add project files.

---
+++ TarenCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TarenCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
